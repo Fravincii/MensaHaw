4e7b041d45b3f72ceab98fa2e75519c8627a5429
Fravincii <fravinci1802@gmail.com>
2023-06-23 16:20:04 +0200

    Viewmodel Test started

--- app/src/test/java/de/haw/mensahaw/Checkout_ViewmodelUnitTest.java
+++ app/src/test/java/de/haw/mensahaw/Checkout_ViewmodelUnitTest.java
@@ -1,4 +1,23 @@
 package de.haw.mensahaw;
 
+import static org.junit.Assert.assertEquals;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+
+import de.haw.mensahaw.viewmodel.Checkout_ViewModel;
 public class Checkout_ViewmodelUnitTest {
+
+    @Test
+    public void get_dishName(){
+        Checkout_ViewModel checkoutViewModel = new Checkout_ViewModel();
+        MutableLiveData<String> expectedResult = new MutableLiveData<>();
+        expectedResult.setValue("Döner");
+
+        checkoutViewModel.setDishNameInView("Döner");
+        MutableLiveData<String> actualResult = checkoutViewModel.getDishName();
+
+        assertEquals(expectedResult.getValue(),actualResult.getValue());
+    }
 }
